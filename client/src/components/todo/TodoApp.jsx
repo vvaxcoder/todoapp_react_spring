d9afb758671718bfb9e1f8c973ea857a1555f3bc
valeriivoronkov <scorpirat@gmail.com>
2020-04-25 20:58:36 +0300

    Add scss to the project; -- add universal onChange event for username and password

--- client/src/components/todo/TodoApp.jsx
+++ client/src/components/todo/TodoApp.jsx
@@ -1,28 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import "./style.scss";
 
 class TodoApp extends Component {
-    render() {
-        return (
-            <div>
-                <div className="todo-app">
-                    <LoginComponent />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <div className='todo-app'>
+          <LoginComponent />
+        </div>
+      </div>
+    );
+  }
 }
 
 class LoginComponent extends Component {
-    render() {
-        return (
-            <div>
-                User Name:
-                <input type="text" name="username" />
-                Password: <input type="password" name="password" />
-                <button>Login</button>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handlerChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  //   handlerUsernameChange = event => {
+  //     console.log(event.target.value);
+  //     this.setState({
+  //       username: event.target.value
+  //     });
+  //   };
+
+  //   handlerPasswordChange = event => {
+  //     console.log(event.target.value);
+  //     this.setState({
+  //       password: event.target.value
+  //     });
+  //   };
+
+  render() {
+    const {
+      state,
+    //   handlerUsernameChange,
+    //   handlerPasswordChange,
+      handlerChange
+    } = this;
+
+    return (
+      <div className='login-wrapper'>
+        User Name:
+        <input
+          type='text'
+          name='username'
+          value={state.username}
+          //   onChange={handlerUsernameChange}
+          onChange={handlerChange}
+        />
+        Password:
+        <input
+          type='password'
+          name={state.password}
+          // onChange={handlerPasswordChange}
+          onChange={handlerChange}
+        />
+        <button className='btn'>Login</button>
+      </div>
+    );
+  }
 }
 
 export default TodoApp;
